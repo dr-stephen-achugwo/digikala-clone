a81178feccb93c417e033b4de6e1fddaab0761ba
Stephen Achugwo <dr_stephen@aol.com>
2023-05-10 09:11:44 +0100

    Add files via upload

---
+++ models/User.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  address: { type: Object },
+  mobile: { type: String },
+  role: { type: String, default: "user" },
+  root: { type: Boolean, default: false },
+});
+
+const User = mongoose.models.user || mongoose.model("user", userSchema);
+
+export default User;
